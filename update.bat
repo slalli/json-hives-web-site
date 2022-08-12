fe1fba6a71ebf6b0eb9396607c6c010cfe41ee34
stefano lalli <stefano@dnasoft.nl>
2022-08-12 19:17:00 +0200

    init

--- update.bat
+++ update.bat
@@ -3,3 +3,11 @@ docker commit json-hives-web-site-wordpress-1 dnasoft/json-hives-web-site-wordpr
 
 docker push dnasoft/json-hives-web-site-mariadb:%1
 docker push dnasoft/json-hives-web-site-wordpress:%1
+
+
+
+
+
+docker run -volumes-from=wordpress-1 -v C:\Users\stefa\OneDrive\jsonHIVES\backup:/tmp dnasoft/json-hives-web-site-wordpress tar czf /tmp/wordpress_data_backup.tar.gz -C /bitnami/wordpress .
+
+docker run -volumes-from=mariadb-1 -v C:\Users\stefa\OneDrive\jsonHIVES\backup:/tmp dnasoft/json-hives-web-site-mariadb tar czf /tmp/mariadb_data_backup.tar.gz -C /bitnami/mariadb .
